24cff4b8940bd253dcb46381a3416b081e8ba935
khakisage <jongjin201@gmail.com>
2024-06-13 02:06:55 +0900

    ✨ 게시글 수정 페이지
    - isEditing, postData 존재 시 수정 모드로 변경
    
    #10

---
+++ src/pages/PostModifyPage.jsx
@@ -0,0 +1,17 @@
+import { useLocation } from 'react-router-dom';
+import GlobalLayout from '../components/layout/GlobalLayout';
+import SideMenu from '../components/community/SideMenu';
+import PostArticleForm from '../components/community/PostArticleForm';
+
+function PostModifyPage() {
+  const { state } = useLocation();
+  const { isEditing, postData } = state;
+  return (
+    <GlobalLayout>
+      <SideMenu />
+      <PostArticleForm isEditing={isEditing} postData={postData} />
+    </GlobalLayout>
+  );
+}
+
+export default PostModifyPage;
