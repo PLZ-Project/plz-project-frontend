27f55faff71738adad07865af93054accc37fe91
khakisage <jongjin201@gmail.com>
2024-06-12 16:23:02 +0900

    ✨ Router 추가
    - Main Page 라우터 추가
    
    #7

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import AuthPage from './pages/AuthPage';
+import MainPage from './pages/MainPage';
 
 function Router() {
   return (
     <Routes>
+      <Route path="/" element={<MainPage />} />
       <Route path="/auth" element={<AuthPage />} />
     </Routes>
   );
