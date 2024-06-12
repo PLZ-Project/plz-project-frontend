0ba0b565ecaf957be736fb48f36b6b23dabd4d78
khakisage <jongjin201@gmail.com>
2024-06-13 02:07:19 +0900

    ✨ Router 추가
    - 게시글 작성 페이지
    - 게시글 수정 페이지
    
    #10

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import AuthPage from './pages/AuthPage';
 import MainPage from './pages/MainPage';
+import PostArticlePage from './pages/PostArticlePage';
+import PostModifyPage from './pages/PostModifyPage';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/auth" element={<AuthPage />} />
+      <Route path="/post" element={<PostArticlePage />} />
+      <Route path="/post/:id" element={<PostModifyPage />} />
     </Routes>
   );
 }
