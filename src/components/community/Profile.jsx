4145bb8bc162399dbde132ed72867f2e68f3d3cf
khakisage <jongjin201@gmail.com>
2024-06-18 19:37:10 +0900

    ⚡️ 공용 사이드바 프로필 컴포넌트 수정
    - css 수정
    - 닉네임 축약 구현
    - 수정 페이지 이동 구현

--- src/components/community/Profile.jsx
+++ src/components/community/Profile.jsx
@@ -1,7 +1,34 @@
+import { useNavigate } from 'react-router-dom';
+
 function Profile() {
+  const navigate = useNavigate();
+  const userinfo = JSON.parse(localStorage.getItem('userInfo'));
+  const nickname = userinfo.nickname;
+  const email = userinfo.email;
+
+  const userNickName = () => {
+    const regex = /^(.*shibaDog\d{0,4}).*/;
+    const match = nickname.match(regex);
+    const name = match ? match[1] : nickname.replace(/^\d{0,4}/, '').slice(0, 12);
+    return name;
+  };
+
+  const goToMyPage = () => {
+    navigate('/userinfo');
+  };
   return (
-    <div className="flex h-[4.5rem] w-[20.5rem] flex-col bg-white">
-      <h1>Profile</h1>
+    <div className="w-82 relative z-[2] flex h-auto flex-col items-center justify-center rounded-t-2xl bg-white">
+      <div className="z-[2] mx-6 mt-10 flex flex-col items-center justify-center gap-6">
+        <img src="/src/assets/dva.png" className="h-24 w-24 rounded-full" />
+        <p className="relative flex flex-col gap-2 text-2xl">
+          {userNickName()}
+          <span onClick={goToMyPage} className="bg-sky rounded-2xl px-2 py-1 text-center text-xs">
+            수정
+          </span>
+        </p>
+        <p className="mb-2 text-opacity-50">{email}</p>
+      </div>
+      <div className="absolute bottom-0 left-0 z-[1] block h-[8.25rem] w-full bg-[#daefff] content-['']"></div>
     </div>
   );
 }
