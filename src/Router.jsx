b9162d5b445d347c5bf86ccb4202ec589362a124
khakisage <jongjin201@gmail.com>
2024-06-23 00:48:35 +0900

    ♻️ 리팩토링
    - 레이지 로딩 적용
    - 대용량 이미지 preload 처리

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,62 +1,66 @@
 import { Route, Routes } from 'react-router-dom';
-import MainPage from './pages/MainPage';
-import PostArticlePage from './pages/PostArticlePage';
-import PostModifyPage from './pages/PostModifyPage';
+import { Suspense, lazy } from 'react';
 import { AuthRoute, NonAuthRoute } from './router/customRoutes';
-import UserinfoPage from './pages/UserinfoPage';
-import PostSpecPage from './pages/PostSpecPage';
-import SigninPage from './pages/SigninPage';
-import SignupPage from './pages/SignupPage';
-import LandingPage from './pages/LandingPage';
+
+const MainPage = lazy(() => import('./pages/MainPage'));
+const PostArticlePage = lazy(() => import('./pages/PostArticlePage'));
+const PostModifyPage = lazy(() => import('./pages/PostModifyPage'));
+const UserinfoPage = lazy(() => import('./pages/UserinfoPage'));
+const PostSpecPage = lazy(() => import('./pages/PostSpecPage'));
+const SigninPage = lazy(() => import('./pages/SigninPage'));
+const SignupPage = lazy(() => import('./pages/SignupPage'));
+const LandingPage = lazy(() => import('./pages/LandingPage'));
 
 function Router() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/main" element={<MainPage />} />
-      <Route
-        path="/login"
-        element={
-          <NonAuthRoute>
-            <SigninPage />
-          </NonAuthRoute>
-        }
-      />
-      <Route
-        path="/signup"
-        element={
-          <NonAuthRoute>
-            <SignupPage />
-          </NonAuthRoute>
-        }
-      />
-      <Route
-        path="/post"
-        element={
-          <AuthRoute>
-            <PostArticlePage />
-          </AuthRoute>
-        }
-      />
-      <Route path="/post/:id" element={<PostSpecPage />} />
-      <Route
-        path="/post/modify/:id"
-        element={
-          <AuthRoute>
-            <PostModifyPage />
-          </AuthRoute>
-        }
-      />
-      <Route
-        path="/userinfo"
-        element={
-          <AuthRoute>
-            <UserinfoPage />
-          </AuthRoute>
-        }
-      />
-      <Route path="/post/:id" element={<PostSpecPage />} />
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/main" element={<MainPage />} />
+        <Route
+          path="/login"
+          element={
+            <NonAuthRoute>
+              <SigninPage />
+            </NonAuthRoute>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <NonAuthRoute>
+              <SignupPage />
+            </NonAuthRoute>
+          }
+        />
+        <Route
+          path="/post"
+          element={
+            <AuthRoute>
+              <PostArticlePage />
+            </AuthRoute>
+          }
+        />
+        <Route path="/post/:id" element={<PostSpecPage />} />
+        <Route
+          path="/post/modify/:id"
+          element={
+            <AuthRoute>
+              <PostModifyPage />
+            </AuthRoute>
+          }
+        />
+        <Route
+          path="/userinfo"
+          element={
+            <AuthRoute>
+              <UserinfoPage />
+            </AuthRoute>
+          }
+        />
+        <Route path="/post/:id" element={<PostSpecPage />} />
+      </Routes>
+    </Suspense>
   );
 }
 
--- src/components/layout/GlobalLayout.jsx
+++ src/components/layout/GlobalLayout.jsx
@@ -1,14 +1,11 @@
 import Banner from '../common/Banner';
 import Header from '../common/Header';
 import SideMenu from '../community/SideMenu';
+import './layout.css';
 
 function GlobalLayout({ children }) {
   return (
-    <div
-      id="globalDiv"
-      className="bg-cover"
-      style={{ backgroundImage: "url('./src/assets/board.webp" }}
-    >
+    <div id="globalDiv" className="bg-cover">
       <Header />
       <div className="flex h-[calc(100rem-72px)] w-full flex-row justify-center bg-cover">
         {/* <Banner /> */}
